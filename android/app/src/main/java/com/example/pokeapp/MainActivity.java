e3b6d58e5ad952735cdd12457ad393008515b912
baike1203 <baike1203@gmail.com>
2026-07-30 22:27:22 +0800

    fix: 补 import android.view.View + CI 改手动装 Android SDK

--- android/app/src/main/java/com/example/pokeapp/MainActivity.java
+++ android/app/src/main/java/com/example/pokeapp/MainActivity.java
@@ -8,6 +8,7 @@ import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.content.ContentUris;
 import android.database.Cursor;
 import android.provider.DocumentsContract;
